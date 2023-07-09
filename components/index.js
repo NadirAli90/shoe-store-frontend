4f11923a125779328db59c520a2f6afc8af1c5fb
NadirAli <nadirali@NadirAlis-MacBook-Pro.local>
2023-07-09 01:01:35 -0700

    initial commit

---
+++ components/index.js
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Wrapper from "./Wrapper";
+import Menu from "./Menu";
+import MenuMobile from "./MenuMobile";
+import HeroBanner from "./HeroBanner";
+import ProductCard from "./ProductCard";
+import ProductDetailsCarousel from "./ProductDetailsCarousel";
+import RelatedProducts from "./RelatedProducts";
+import CartItem from "./CartItem";
+
+export {
+  Header,
+  Footer,
+  Wrapper,
+  Menu,
+  MenuMobile,
+  HeroBanner,
+  ProductCard,
+  ProductDetailsCarousel,
+  RelatedProducts,
+  CartItem,
+};
